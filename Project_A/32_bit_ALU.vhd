b474542fe3ba5b8de7f21ce477969569f34292ac
jrvaughn <jrvaughn@iastate.edu>
2019-10-06 17:11:54 -0500

    1bit ALU done

--- Project_A/32_bit_ALU.vhd
+++ Project_A/32_bit_ALU.vhd
@@ -3,10 +3,26 @@
 -- Nickolas Mitchell
 -------------------------------------------------------------------------
 
--- ALU_1_bit.vhd
+-- 32_bit_ALU.vhd
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+entity 32_bit_ALU is
+  port(i_a       : in std_logic;
+       i_b       : in std_logic;
+       less      : in std_logic;
+       ainvert   : in std_logic;
+       binvert   : in std_logic;
+       Carryin   : in std_logic;
+       Operation : in std_logic_vector (2 downto 0);
+       Carryout  : out std_logic;
+       result    : out std_logic);
+       
+end 32_bit_ALU;
+
+architecture arch of 32_bit_ALU is
+
+--components
 entity ALU_1_bit is
   port(i_a       : in std_logic;
        i_b       : in std_logic;
@@ -19,19 +35,6 @@ entity ALU_1_bit is
        result    : out std_logic);
        
 end ALU_1_bit;
-
-architecture arch of ALU_1_bit is
-
---components
-component mux_2to1_1bit
-
-  port(i_0, i_1          : in std_logic;
-       i_sel		 : in std_logic;  
-       o_F     	         : out std_logic);
-
-end component;
-
-component mux_5to1_1bit
 
   port(i_0, i_1, i_2, i_3, i_4          : in std_logic;
        i_sel				: in std_logic_vector(2 downto 0); 
