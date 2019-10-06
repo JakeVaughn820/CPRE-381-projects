c8a5a11c53788cac53eef9c27d552e9b7258eb65
jrvaughn <jrvaughn@iastate.edu>
2019-10-06 18:03:35 -0500

    Part 2 almost done

--- Project_A/32_bit_ALU.vhd
+++ Project_A/32_bit_ALU.vhd
@@ -8,22 +8,19 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity 32_bit_ALU is
-  port(i_a       : in std_logic;
-       i_b       : in std_logic;
-       less      : in std_logic;
-       ainvert   : in std_logic;
-       binvert   : in std_logic;
-       Carryin   : in std_logic;
-       Operation : in std_logic_vector (2 downto 0);
-       Carryout  : out std_logic;
-       result    : out std_logic);
-       
+  port(A         : in   std_logic_vector(31 downto 0);
+       B         : in   std_logic_vector(31 downto 0);
+       control   : in   std_logic_vector(5 downto 0);
+       F         : out  std_logic_vector(31 downto 0);
+       Carryout  : out  std_logic;
+       Overflow  : out  std_logic;
+       Zero      : out  std_logic; 
 end 32_bit_ALU;
 
 architecture arch of 32_bit_ALU is
 
 --components
-entity ALU_1_bit is
+component ALU_1_bit
   port(i_a       : in std_logic;
        i_b       : in std_logic;
        less      : in std_logic;
@@ -32,76 +29,65 @@ entity ALU_1_bit is
        Carryin   : in std_logic;
        Operation : in std_logic_vector (2 downto 0);
        Carryout  : out std_logic;
-       result    : out std_logic);
-       
-end ALU_1_bit;
-
-  port(i_0, i_1, i_2, i_3, i_4          : in std_logic;
-       i_sel				: in std_logic_vector(2 downto 0); 
-       o_F          			: out std_logic);
-
+       result    : out std_logic);       
 end component;
 
-component fulladder
-  port(i_A1, i_B1  : in std_logic;
-       i_Cin  : in std_logic;
-       o_carry : out std_logic; 
-       o_sum  : out std_logic);
-
+component ALU_1_bit_MostSig
+  port(i_a       : in std_logic;
+       i_b       : in std_logic;
+       less      : in std_logic;
+       ainvert   : in std_logic;
+       binvert   : in std_logic;
+       Carryin   : in std_logic;
+       Operation : in std_logic_vector (2 downto 0);
+       Carryout  : out std_logic;
+       result    : out std_logic;
+       Set       : out std_logic;
+       Overflow  : out std_logic);      
 end component;
 
  --signals
-signal      s_ainvert_out    : std_logic;
-signal      s_binvert_out    : std_logic;
-signal      s_a_and_b        : std_logic;
-signal      s_a_or_b         : std_logic;
-signal      s_a_nand_b       : std_logic;
-signal      s_a_xor_b        : std_logic;
-signal      s_sum            : std_logic;
-signal      s_notA           : std_logic;
-signal      s_notB           : std_logic;
-
+signal       s_less      : std_logic;
+signal       s_ainvert   : std_logic;
+signal       s_binvert   : std_logic;
+signal       s_Carryin   : std_logic;
+signal       s_Operation : std_logic_vector (2 downto 0);
+signal       s_Set       : std_logic;
 
 begin
 
-s_notA <= NOT(i_a);
-s_notB <= NOT(i_b);
+       s_less <= Set;
+       s_ainvert <= control(5);
+       s_binvert <= control(4);
+       s_Carryin <= control(3);
+       s_Operation <= control(2 downto 0);
 
-Mux_ainvert: mux_2to1_1bit
-  port map(i_0 => i_a,
-          i_1 => s_notA,
-          i_sel => ainvert,
-          o_F => s_ainvert_out);
+G1: for i in 0 to 30 generate
 
-Mux_binvert: mux_2to1_1bit
-  port map(i_0 => i_b,
-          i_1 => s_notB,
-          i_sel => binvert,
-          o_F => s_binvert_out);
-  
-fulladder1: fulladder
-  port map(i_A1 => s_ainvert_out,
-       i_B1 => s_binvert_out,
-       i_Cin => Carryin,
-       o_carry => Carryout,
-       o_sum => s_sum);
+ALU_1_bitX: ALU_1_bit
+  port map(i_a => A(i),
+           i_b => B(i),
+           less => 0,
+           ainvert => s_ainvert,
+           binvert => s_binvert,
+           Carryin => s_Carryin,
+           Operation => s_Operation,
+           Carryout => s_Carryin,
+           result => F(i));  
 
- s_a_and_b <= s_ainvert_out AND s_binvert_out;
+end generate;
 
- s_a_or_b <= s_ainvert_out OR s_binvert_out;
-
- s_a_nand_b <= s_ainvert_out NAND s_binvert_out;
-
- s_a_xor_b <= s_a_nand_b AND s_a_or_b;
-
-resultMux: mux_5to1_1bit
-  port map(i_0 => s_a_and_b,
-       i_1 => s_a_or_b,
-       i_2 => s_sum,
-       i_3 => s_a_xor_b,
-       i_4 => less,
-       i_sel => Operation,
-       o_F => result);
-
+My_ALU_1_bit_MostSig: ALU_1_bit_MostSig
+  port map(i_a => A(31),
+           i_b => B(31),
+           less => 0,
+           ainvert => s_ainvert,
+           binvert => s_binvert,
+           Carryin => s_Carryin,
+           Operation => s_Operation,
+           Carryout => Carryout,
+           result => F(i),
+           Set => s_Set,
+           Overflow => Overflow);
 
 end arch;
