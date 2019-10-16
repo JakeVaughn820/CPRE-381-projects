29457c99f7ebcb34ed9562d947c349893ac803de
nickisu <nickisu@iastate.edu>
2019-10-16 13:04:31 -0500

    shifter

--- Project_A/right_left_shifter.vhd
+++ Project_A/right_left_shifter.vhd
@@ -34,56 +34,72 @@ end component;
 signal extend : std_logic; 
 
 --Outputs
-signal s_mux0, s_mux_1, s_mux2, s_mux3 : std_logic_vector(31 downto 0);
+signal s_mux0, s_mux1, s_mux2, s_mux3 : std_logic_vector(31 downto 0);
 
-signal s_shift : std_logic_vector(31 downto 0);
+signal s_shift, s_shift_temp, o_shift_temp : std_logic_vector(31 downto 0);
 
 signal temp_shift : std_logic_vector(31 downto 0);
 
+signal s1_0, s1_1, s1_2, s1_3, s1_4 : std_logic_vector(31 downto 0); 
  
-
 begin
+	
+	gen0: for i in 0 to 31 generate 
+	s_shift_temp(i) <= i_shift(31-i); 
+	end generate;  
 
-	extend <= i_shift(31) when (ctl_which_shift == "00") else '0';
-	s_shift <= i_shift(0 to 31) when (ctl_which_shift == "10") else i_shift; 
-
-	 
+	extend <= i_shift(31) when (ctl_which_shift = "00") else '0';
+	s_shift <= s_shift_temp when (ctl_which_shift = "10") else i_shift; 
 
 ---------------------------------------------------------------
 -- Shift right arthmetic & logical
 ---------------------------------------------------------------
 
+	s1_0 <= extend & s_shift(31 downto 1); 
 	task1 : mux_2to1_32bit
        port MAP(i_0	=> s_shift,
-		i_1 	=> extend $ s_shift(31 downto 1),
+		i_1 	=> s1_0,
 		sel	=> ctl_bits_to_shift(0),
 		o_f	=> s_mux0);
 
+
+	s1_1 <= extend & extend & s_mux0(31 downto 2);
 	task2 : mux_2to1_32bit
        port MAP(i_0	=> s_mux0,
-		i_1 	=> extend $ extend $ s_mux0(31 downto 2),
+		i_1 	=> s1_1,
 		sel	=> ctl_bits_to_shift(1),
 		o_f	=> s_mux1);
 
+
+	s1_2 <= extend & extend & extend & extend & s_mux1(31 downto 4);
 	task3 : mux_2to1_32bit
        port MAP(i_0	=> s_mux1,
-		i_1 	=> extend $ extend $ extend $ extend $ s_mux1(31 downto 4),
+		i_1 	=> s1_2,
 		sel	=> ctl_bits_to_shift(2),
 		o_f	=> s_mux2);
 
+
+	s1_3 <= extend & extend & extend & extend & extend & extend & extend & extend & s_mux2(31 downto 8);
 	task4 : mux_2to1_32bit
        port MAP(i_0	=> s_mux2,
-		i_1 	=> extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ s_mux2(31 downto 8),
+		i_1 	=> s1_3,
 		sel	=> ctl_bits_to_shift(3),
 		o_f	=> s_mux3);
 
+
+	s1_4 <= extend & extend & extend & extend & extend & extend & extend & extend & extend & extend & extend & extend & extend & extend & extend & extend & s_mux3(31 downto 16);
 	task5 : mux_2to1_32bit
        port MAP(i_0	=> s_mux3,
-		i_1 	=> extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ extend $ s_mux3(31 downto 16),
+		i_1 	=> s1_4,
 		sel	=> ctl_bits_to_shift(4),
 		o_f	=> temp_shift);
 
-	o_shift <= temp_shift(0 to 31) when (ctl_which_shift == "10") else temp_shift; 
+
+	gen1: for i in 0 to 31 generate 
+	o_shift_temp(i) <= temp_shift(31-i); 
+	end generate;
+
+	o_shift <= o_shift_temp when (ctl_which_shift = "10") else temp_shift; 
 
 
 
