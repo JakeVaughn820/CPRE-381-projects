0766719530e915d0fbbf09387f87b00d86bf6d8c
jrvaughn <jrvaughn@iastate.edu>
2019-11-07 13:20:53 -0600

    fixed buggs errors and starting testing.

--- Project_B/cpre381-toolflow-release/ModelSimWork/src/Control.vhd
+++ Project_B/cpre381-toolflow-release/ModelSimWork/src/Control.vhd
@@ -59,11 +59,12 @@ s_code <= i_fnCode when (i_opCode = "000000") else i_opCode;
 	--1 for                         sw
 	o_MemWrite <= '1' when(s_code = "101011") else '0';
 
-	--1 for                            addi                 addiu               andi                  lui                  xori                 ori                  slti               sltiu                 sw                    lw
-	--o_ALUSrc <= '1' when(s_code = "001000" or s_code = "001001" or s_code = "001100" or s_code = "001111" or s_code = "001110" or s_code = "001101" or s_code = "001010" or s_code = "001011" or s_code = "101011" or s_code = "100011") else '0';
+	--1 for                         addi                 addiu               andi                  lui                  xori                 ori                  slti               sltiu                 sw                    lw
+	o_ALUSrc <= '1' when(s_code = "001000" or s_code = "001001" or s_code = "001100" or s_code = "001111" or s_code = "001110" or s_code = "001101" or s_code = "001010" or s_code = "001011" or s_code = "101011" or s_code = "100011") else '0';
 
-	--0 for                        sw                   beq                bne                  j                    jr
-	o_ReWrite <= '0' when(s_code = "101011" or s_code = "000100" or s_code = "000101" or s_code = "000010" or s_code = "001000") else '1';
+	--0 for                          sw                   beq                bne                  j                    jr
+	o_ReWrite <= '0' when(s_code = "101011") else '1';
+--	or s_code = "000100" or s_code = "000101" or s_code = "000010" or s_code = "001000") else '1';
 
 	--0 for                         sllv                 srlv                 srav
 	o_Shift <= '0' when(s_code = "000100" or s_code = "000110" or s_code = "000111") else '1';
